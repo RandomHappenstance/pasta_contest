c73fb90e6f87a7b8376cc4d36c697f6a2941a403
Alan Autrand <alan.autrand@gmail.com>
2017-09-14 22:11:22 -0700

    inital commit

---
+++ pasta.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+from bs4 import BeautifulSoup as BS
+import urllib
+
+driver = webdriver.Firefox()
+
+disabled = True
+
+# while disabled:
+# 	url = urllib.urlopen('http://www.pastapass.com')
+# 	soup = BS(url)
+# 	a = soup.find('a', {'id':'ppp'})
+# 	print('disabled' in a['class'])
+# 	if 'disabled' not in a["class"]:
+# 		print('here')
+# 		disabled = False
+
+# driver.get('https://www.pastapass.com/')
+
+# elem = driver.find_element_by_id('ppp')
+# elem.click()
+
